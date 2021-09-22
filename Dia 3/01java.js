ad96f71f4105c8eef5f63756c702eea073bfad29
agilio <ariabrigi@gmail.com>
2021-09-21 21:28:54 -0500

    grid

---
+++ Dia 3/01java.js
@@ -0,0 +1,10 @@
+console.log("Hola Mundo!");
+var nombre = "Ariana";
+console.log(nombre);
+var nombre2 = "Ariana",
+var edad = 31;
+console.log(edad);
+var esDeNoche = true;
+console.log(esDeNoche);
+edad =25;
+console.log(edad);
